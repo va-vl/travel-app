00c195aa5b124bbd3d87b715148166ad98848030
va-z <vasilizadorojnyuk@gmail.com>
2021-03-06 16:37:39 +0500

    init: start travel-app task

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:jest/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jest'],
+  rules: {
+    'no-underscore-dangle': ['error', { allowAfterThis: true }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-curly-newline': 'off',
+    'react/jsx-props-no-spreading': 'off',
+  },
+};
