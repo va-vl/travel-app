30d8807a68161ec40eba6ee9ad347db2c07d157a
va-z <vasilizadorojnyuk@gmail.com>
2021-03-07 22:47:15 +0500

    fix: fix merge bugs

--- src/components/Header/LanguageSelect/LanguageSelect.jsx
+++ src/components/Header/LanguageSelect/LanguageSelect.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-<<<<<<< HEAD
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 import { useLanguage, useLanguageUpdate } from '../../../contexts/LanguageContext';
 import { languageOptions } from '../../../constants/index';
 
@@ -8,48 +9,23 @@ const LanguageSelect = () => {
   const setLanguage = useLanguageUpdate();
 
   const handleChange = ({ target: { value } }) => { setLanguage(value); };
-=======
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import { useLanguage } from '../../../utils/localStorage';
-import LANGUAGES from '../../../constants/languages';
-
-const LanguageSelect = () => {
-  const [language, setLanguage] = useLanguage('en');
-  const handleChange = ({ target: { value } }) => setLanguage(value);
-  const languagesArr = LANGUAGES.map((lang) => (
-    <option
-      value={lang}
-      key={lang}
-    >
-      {lang}
-    </option>
-  ));
->>>>>>> develop
 
   return (
     <FormControl
       variant="outlined"
       size="small"
     >
-<<<<<<< HEAD
-      {
-        Object.entries(languageOptions).map(([lang, name]) => (
-          <option value={lang} key={lang}>{name}</option>
-        ))
-      }
-    </select>
-=======
       <Select
         native
         variant="outlined"
         value={language}
         onChange={handleChange}
       >
-        {languagesArr}
+        {Object.entries(languageOptions).map(([lang, name]) => (
+          <option value={lang} key={lang}>{name}</option>
+        ))}
       </Select>
     </FormControl>
->>>>>>> develop
   );
 };
 
