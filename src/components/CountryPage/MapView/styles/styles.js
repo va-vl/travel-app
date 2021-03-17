d836c751bc9fcb2db5fd2884cc04f2b60fcd533b
va-z <vasilizadorojnyuk@gmail.com>
2021-03-17 12:49:41 +0500

    fix: map build bug

--- src/components/CountryPage/MapView/styles/styles.js
+++ src/components/CountryPage/MapView/styles/styles.js
@@ -1,14 +1,13 @@
 import { makeStyles } from '@material-ui/core';
 
 const styles = makeStyles({
-  'fullscreen-control': {
+  'controls-wrapper': {
+    position: 'relative',
+    width: '29px',
     top: '10px',
     left: '10px',
   },
-  'navigation-control': {
-    top: '46px',
-    left: '10px',
-  },
+
 });
 
 export default styles;
